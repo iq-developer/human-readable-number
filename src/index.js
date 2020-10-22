b3be6f83c83854ecc158482f7b46aa59b9c4f660
Vladimir Sevastianov <108.nimay@gmail.com>
2020-10-22 16:41:55 +0300

    refactor: change structure of the project

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 //module.exports = function toReadable(number) {
 function toReadable(number) {
   number = '' + number;
-  let units1_9 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  let units20_90 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  let unitsBig = ['hundred', 'thousand', 'million', 'billion', 'trillion'];
+  let arrFirsts = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let arrTens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let arrBig = ['hundred', 'thousand', 'million', 'billion', 'trillion'];
   let result = '';
   let ones = number[number.length-1];
   let tens = number[number.length-2];
   let hundreds = number[number.length-3];
+  let thousands = number.slice(0, -3);
 
   if (number === 0){
     result = 'zero';
 
   } else if (number < 20){
-    result = units1_9[number];
+    result = arrFirsts[number];
 
   } else if (number < 100){
-    tens = ('' + number)[0];
-    ones = ('' + number)[1];
-    result = units20_90[tens] + ' ' + units1_9[ones];
+    result = arrTens[tens] + ' ' + arrFirsts[ones];
 
   }  else if (number < 1000){
-    hundreds = ('' + number)[0];
-    tens = ('' + number)[1];
-    ones = ('' + number)[2];
-    result = units1_9[hundreds] + ' ' + unitsBig[0] + ' ' + units20_90[tens] + ' ' + units1_9[ones];
+    result = arrFirsts[hundreds] + ' ' + arrBig[0] + ' ' + arrTens[tens] + ' ' + arrFirsts[ones];
   }
 
   else if (number < 1000000){
-    thousands = ('' + number).slice(0, -3);
     thousandsReadable = toReadable(thousands);
 
   }
@@ -36,4 +31,4 @@ function toReadable(number) {
   return result;
 }
 
-console.log ( toReadable(123456) );
+console.log ( toReadable(123) );
